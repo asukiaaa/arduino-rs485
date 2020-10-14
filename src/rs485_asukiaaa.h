14ac4f233eb2064fefcec8d7e15e7b3d33a8a8a6
Asuki Kono <asukiaaa@gmail.com>
2020-10-14 12:25:51 +0900

    Apply Modbus namespace

--- src/rs485_asukiaaa.h
+++ src/rs485_asukiaaa.h
@@ -4,28 +4,35 @@
 #include <Arduino.h>
 
 namespace rs485_asukiaaa {
-  enum ModbusError { NoResponse = 11,
-                     UnmatchCrc = 12,
-                     UnmatchAddress = 13,
-                     UnmatchFnCode = 14,
-                     UnmatchDataLen = 15,
-                     OverQueryMaxLen = 16,
-  };
+  namespace ModbusRtu {
+    enum FnCode { Read       = 0x03,
+                  Write      = 0x06,
+                  WriteMulti = 0x10,
+    };
 
-  class Modbus {
-   public:
-    Modbus(HardwareSerial* serial, int16_t pinDe, int16_t pinRe);
-    void begin(long baudrate);
-    void beginWithoutSerial();
-    void writeQuery(uint8_t address, uint8_t fnCode, uint8_t* data, uint16_t dataLen);
-    uint8_t readQuery(uint8_t address, uint8_t fnCode, uint8_t* data, uint16_t dataLen, unsigned long msTimeout = 50UL);
-   private:
-    HardwareSerial* serial;
-    int16_t pinDe;
-    int16_t pinRe;
-  };
+    enum Error { NoResponse = 11,
+                 UnmatchCrc = 12,
+                 UnmatchAddress = 13,
+                 UnmatchFnCode = 14,
+                 UnmatchDataLen = 15,
+                 OverQueryMaxLen = 16,
+    };
 
-  uint16_t createCRC16(const uint8_t* data, uint16_t dataLen);
+    class Central {
+    public:
+      Central(HardwareSerial* serial, int16_t pinDe, int16_t pinRe);
+      void begin(long baudrate);
+      void beginWithoutSerial();
+      void writeQuery(uint8_t address, uint8_t fnCode, uint8_t* data, uint16_t dataLen);
+      uint8_t readQuery(uint8_t address, uint8_t fnCode, uint8_t* data, uint16_t dataLen, unsigned long msTimeout = 50UL);
+    private:
+      HardwareSerial* serial;
+      int16_t pinDe;
+      int16_t pinRe;
+    };
+
+    uint16_t createCRC16(const uint8_t* data, uint16_t dataLen);
+  }
 }
 
 #endif
