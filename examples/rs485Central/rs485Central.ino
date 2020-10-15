a0c27fedce97b590d830a7f666cb9e2b65c55899
Asuki Kono <asukiaaa@gmail.com>
2020-10-15 17:27:44 +0900

    Update example

--- examples/rs485Central/rs485Central.ino
+++ examples/rs485Central/rs485Central.ino
@@ -1,13 +1,16 @@
 #include <rs485_asukiaaa.h>
 
-#define PIN_DE 5
-#define PIN_RE 6
-#define BAUDRATE 115200
-rs485_asukiaaa::Modbus modbus(&Serial1, PIN_DE, PIN_RE);
+#define PIN_RS485_DE 5
+#define PIN_RS485_RE 6
+#define RS485_BAUDRATE 115200
+#define RS485_DEVICE_ADDRESS 2
+rs485_asukiaaa::ModbusRtu::Central modbus(&Serial1, PIN_RS485_DE, PIN_RS485_RE);
 
 void setup() {
-  modbus.begin(BAUDRATE);
+  modbus.begin(RS485_BAUDRATE);
 }
 
 void loop() {
+  uint8_t data[] = { 0, 1, 2 };
+  modbus.writeQuery(RS485_DEVICE_ADDRESS, rs485_asukiaaa::ModbusRtu::FnCode::Write, data, 3);
 }
