0951758271373e5ce20320ea8c872138e7405269
Asuki Kono <asukiaaa@gmail.com>
2022-06-30 17:04:44 +0900

    Update version to 1.2.5

--- src/rs485_asukiaaa.h
+++ src/rs485_asukiaaa.h
@@ -5,7 +5,7 @@
 
 #define RS485_ASUKIAAA_VERSION_MAJOR 1
 #define RS485_ASUKIAAA_VERSION_MINOR 2
-#define RS485_ASUKIAAA_VERSION_PATCH 4
+#define RS485_ASUKIAAA_VERSION_PATCH 5
 
 namespace rs485_asukiaaa {
 namespace ModbusRtu {
